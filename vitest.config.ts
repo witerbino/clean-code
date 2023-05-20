c3404595718136b98cd512e17a929f559361a4b2
Wallace Viterbino <witerbino@gmail.com>
2023-05-20 00:22:16 -0300

    chore: modify config vitest

--- vitest.config.ts
+++ vitest.config.ts
@@ -2,8 +2,5 @@ import { defineConfig } from 'vitest/config';
 import tsconfigPaths from 'vite-tsconfig-paths';
 
 export default defineConfig({
-  test: {
-    reporters: 'verbose',
-  },
   plugins: [tsconfigPaths()],
 });
